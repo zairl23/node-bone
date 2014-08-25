42aa0f715e0e524b5d22732ac383364b384e9194
root <admin@local.host>
2014-08-25 23:13:18 +0800

    add Model login from one api

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ var koa = require('koa')
   , router = require('koa-router')
   , request = require('request')
   , app = koa()
-  , serve = require('koa-static');
+  , serve = require('koa-static')
+  , url = 'http://example.com';
+
+// Model logic
+var Model = {
+    all: function() {
+		request(apiName, function (error, response, body){
+			if (!error && response.statusCode == 200) {
+    		    Model.data = '';
+		        Model.data = JSON.parse(body);
+    	    }
+   	});
+   	return Model.data;
+} 
 
 app.use(router(app));
 // server static file
@@ -17,7 +30,10 @@ app.get('/web/1', function *(next) {
   this.body = 'hello';
   //app.use(serve('index.html'));
 });
-
+// route about Model
+app.get('/model/all', function *(next) {
+    this.body = Model.all();
+});
 
 var port = Number(process.env.PORT || 3000);
 app.listen(port);
