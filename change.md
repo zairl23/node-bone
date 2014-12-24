c5f2f079045cbf7c1eeba23a136371b32d5a343e
neoney <zairl23@126.com>
2014-12-24 15:49:38 +0800

    restruct node-bone

---
+++ change.md
@@ -0,0 +1,15 @@
+2014-12-27
+-----------------------------------------------
+1.  add nodemon to debug codes
+
+2.  add     
+	"koa-mongo": "~0.2.0",
+    	"co-monk": "~1.0.0",
+   	 "monk": "~0.9.1",
+    	"co-body": "~1.0.0"
+
+to connect mongodb
+
+3. add  directory 'www' to place js, css and html front codes
+
+4. add license
--- index.js
+++ index.js
@@ -3,35 +3,38 @@ var koa = require('koa')
   , request = require('request')
   , app = koa()
   , serve = require('koa-static')
+  , apiTodos = require('./api/todos')
   , url = 'http://example.com';
 
 // Model logic
 var Model = {
     all: function() {
-		request(apiName, function (error, response, body){
-			if (!error && response.statusCode == 200) {
-    		    Model.data = '';
-		        Model.data = JSON.parse(body);
-    	    }
-   	    });
-   	    return Model.data;
+        request(apiName, function (error, response, body){
+            if (!error && response.statusCode == 200) {
+                Model.data = '';
+                Model.data = JSON.parse(body);
+            }
+        });
+        return Model.data;
     }
 } 
-
+//lauch route
 app.use(router(app));
 // server static file
-app.use(serve('.'));
+app.use(serve('./www'));
 //app.use('/', )
 app.get('/', function *(next) {
   //var user = yield User.findOne(this.params.id);
   this.body = 'hello, welcome to node-bone';
 });
 
+app.get('/todos', apiTodos.list);
+
 // route about Model
 app.get('/model/all', function *(next) {
     this.body = Model.all();
 });
 
-var port = Number(process.env.PORT || 3000);
+var port = Number(process.env.PORT || 8888);
 app.listen(port);
 
